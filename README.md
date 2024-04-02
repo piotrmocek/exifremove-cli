b036ac86a23a1ac7de0fc85eb8e0fd8b82c6db4b
Piotr Mocek <piotr.mocek@opusb.pl>
2024-04-02 11:25:53 +0200

    Overwrite original jpg file on optimization

---
+++ README.md
@@ -0,0 +1,30 @@
+# exifremove-cli
+
+[![npm](https://img.shields.io/npm/v/exifremove-cli)](https://www.npmjs.com/package/exifremove-cli)
+
+Command-line interface that utilizes [exifremove](https://github.com/Coteh/exifremove) to remove EXIF metdata from JPEG images.
+
+## Installation
+
+### Normal Installation
+
+`npm install -g git+https://github.com/piotrmocek/exifremove-cli.git`
+
+### Development Installation
+
+1. `cd` into project root. (containing the main `exifremove` module)
+1. Run `npm link` to link it to global `node_modules`.
+1. `cd` back into CLI folder, run `npm link exifremove` to link `exifremove` dependency to the linked version made in the previous step.
+1. Run `npm install` to install the remaining CLI dependencies.
+
+## Usage
+
+```
+exifremove [image0] ... [imageN]
+
+Options:
+--version            Show version number                             [boolean]
+-v, --verbose        Print extra messages                              [count]
+--km, --keep-marker  Keeps the APP1 marker in the JPEG               [boolean]
+-h, --help           Show help                                       [boolean]
+```
---
+++ app.js
@@ -0,0 +1,73 @@
+#!/usr/bin/env node
+
+const fs = require("fs");
+const exifRemove = require("exifremove");
+const argv = require("yargs")
+    .usage("exifremove [image0] ... [imageN]")
+    .count("verbose")
+    .alias("v", "verbose")
+    .boolean("keep-marker")
+    .alias("km", "keep-marker")
+    .alias("h", "help")
+    .describe({
+        verbose: "Print extra messages",
+        "keep-marker": "Keeps the APP1 marker in the JPEG",
+    }).argv;
+
+var imageArr = argv._;
+
+if (imageArr.length === 0) {
+    console.error("No images supplied");
+    process.exit(1);
+}
+
+imageArr.forEach((elem) => {
+    var lowercaseName = elem.toLowerCase();
+    if (!lowercaseName.endsWith(".jpg") && !lowercaseName.endsWith(".jpeg")) {
+        console.error("'" + elem + "' is not a JPEG.");
+        process.exit(1);
+    }
+});
+
+var images = imageArr.map((fileName) => {
+    try {
+        return {
+            buffer: fs.readFileSync(fileName),
+            srcFileName: fileName,
+            destFileName: fileName,
+        };
+    } catch (e) {
+        if (e.code === "ENOENT") {
+            console.log("Error opening '" + fileName + "'. Image file does not exist.");
+        } else {
+            console.log("Unknown error opening '" + fileName + "'. Error code: " + e.code);
+        }
+        process.exit(1);
+    }
+});
+
+images.forEach((imageFileInfo) => {
+    var imageFile = imageFileInfo.buffer;
+
+    if (argv.verbose) {
+        console.log("Processing '" + imageFileInfo.srcFileName + "'");
+        if (argv.verbose > 1) console.log(imageFile);
+        console.log("Image file length: " + imageFile.length);
+    }
+
+    var result = exifRemove.remove(imageFileInfo.buffer, {
+        keepMarker: argv.keepMarker,
+        verbose: argv.verbose,
+    });
+
+    if (argv.verbose)
+        console.log("Writing EXIF-stripped image file to '" + imageFileInfo.destFileName + "'...");
+
+    fs.writeFile(imageFileInfo.destFileName, result, (err) => {
+        if (err) {
+            console.error("Error writing file");
+            return;
+        }
+        console.log("File written successfully.");
+    });
+});
